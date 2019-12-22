b3f28340827186dafd8984fa9fbd53fa2fbf255e
jlb1999 <jlbroughton88@gmail.com>
2019-12-22 09:50:15 -0500

    home 3 finished and made responsive

--- client/src/components/Home3/Home3.js
+++ client/src/components/Home3/Home3.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home3.scss";
+import Img1 from "../images/img1.jpg";
 
 const Home3 = () => {
     return (
@@ -7,17 +8,53 @@ const Home3 = () => {
             <div className="home3Main">
                 <div className="overviewSectParent">
                     <div className="overviewSect">
-                        <h2 className="overviewHead">A Brief Overview of H3</h2>
-                        <div className="overviewParaDiv">
-                        <p className="overviewPara">At H3 Staffing we work hard to understand your complex staffing needs and work
-                             efficiently to find the perfect marriage between employer and employee. <br/><br/>  Having
-                              a combined total of 10 years of Staffing experience, our mission is to build
-                               meaningful relationships. It’s what we’re about. <br/><br/> We’ve got connections that
-                               count, experience that makes all the difference, deep working relationships
-                                that matter and the right recruiting team who’s got your back.
+                        <h2 className="overviewHead">A Brief Overview of H3 Staffing</h2>
+
+                        <div className="overviewImgSect2">
+                       <div className="overviewParaDiv2">
+                                <p className="overviewPara2">At H3 Staffing we work hard to understand your complex staffing needs and work
+                                    efficiently to find the perfect marriage between employer and employee. <br/><br/>  Having
+                                    a combined total of 10 years of Staffing experience, our mission is to build
+                                    meaningful relationships. It’s what we’re about. <br/><br/> We’ve got connections that
+                                    count, experience that makes all the difference, deep working relationships
+                                    that matter and the right recruiting team who’s got your back.
                                 </p>
+                            </div>
+                            <img className="overviewImg2" src={Img1}></img>
                         </div>
 
+
+
+
+
+                        <div className="overviewParaDiv">
+                            <p className="overviewPara">At H3 Staffing we work hard to understand your complex staffing needs and work
+                                efficiently to find the perfect marriage between employer and employee. <br/><br/>  Having
+                                a combined total of 10 years of Staffing experience, our mission is to build
+                                meaningful relationships. It’s what we’re about. <br/><br/> We’ve got connections that
+                                count, experience that makes all the difference, deep working relationships
+                                that matter and the right recruiting team who’s got your back.
+                            </p>
+                        </div>
+                        <div className="briefSect">
+                            <div className="briefDiv">
+                                <h3 className="briefHead">We Provide Consistent, Professional, Quality Service</h3>
+                                <p className="briefPara">We do whatever it takes to build client and consultant loyalty.</p>
+                            </div>
+
+                            <div className="briefDiv">
+                                <h3 className="briefHead">We Take Care Of Our People</h3>
+                                <p className="briefPara">We understand that our success is because of our people.</p>
+                            </div>
+
+                            <div className="briefDiv">
+                                <h3 className="briefHead">We Work Together</h3>
+                                <p className="briefPara">By working as a synchronized, organized team; we are able to provide a better, more professional service than anyone else.</p>
+                            </div>
+                        </div>           
+                    </div>                        
+                    <div className="overviewImgSect">
+                        <img className="overviewImg" src={Img1}></img>
                     </div>
                 </div>
             </div>
