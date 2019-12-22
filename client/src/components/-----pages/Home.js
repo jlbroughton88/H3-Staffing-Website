e46f99354c51676890efd0a96282533bfb7a224b
jlb1999 <jlbroughton88@gmail.com>
2019-12-21 19:58:12 -0500

    home3 .js and css initial push

--- client/src/components/-----pages/Home.js
+++ client/src/components/-----pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Home1 from "../Home1/Home1";
 import Home2 from "../Home2/Home2";
+import Home3 from "../Home3/Home3";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar/>
             <Home1/>
             <Home2/>
+            <Home3/>
         </div>
     )
 }
---
+++ client/src/components/Home3/Home3.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home3.scss";
+
+const Home3 = () => {
+    return (
+        <div className="home3Mother">
+            <div className="home3Main">
+                <div className="overviewSectParent">
+                    <div className="overviewSect">
+                        <h2 className="overviewHead">A Brief Overview of H3</h2>
+                        <div className="overviewParaDiv">
+                        <p className="overviewPara">At H3 Staffing we work hard to understand your complex staffing needs and work
+                             efficiently to find the perfect marriage between employer and employee. <br/><br/>  Having
+                              a combined total of 10 years of Staffing experience, our mission is to build
+                               meaningful relationships. It’s what we’re about. <br/><br/> We’ve got connections that
+                               count, experience that makes all the difference, deep working relationships
+                                that matter and the right recruiting team who’s got your back.
+                                </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home3;
