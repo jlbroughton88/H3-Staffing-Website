74aa4810120a5eff9a295d5e52d7f1b47d605681
jlb1999 <jlbroughton88@gmail.com>
2019-12-23 19:14:49 -0500

    scrolling part added

---
+++ client/src/components/Home5/Home5.js
@@ -0,0 +1,172 @@
+import React from "react";
+import "./Home5.scss";
+
+const Home5 = () => {
+    return (
+        <div className="home5Mother">
+            <div className="home5Main">
+
+                <div className="home5HeadDiv">
+                    <h1 className="home5Head">Testimonials</h1>
+                </div>
+
+                <div className="scrollingDiv">
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonialBoxParent">
+                        <div className="testimonialBox">
+                            <p className="testimonialPara">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className="testimonialRoleDiv">
+                            <p className="testimonialRole">Software Engineer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home5;
